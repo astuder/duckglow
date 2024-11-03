95c6cb978328e796e80b8ffebaad6390a7b66801
Adrian Studer <github@adrianstuder.com>
2024-11-03 14:17:03 -0800

    add examples

---
+++ examples/demo-ws2812.py
@@ -0,0 +1,46 @@
+import neopixel
+import time
+
+# GPIO2 of duck (gpio_2) for WS2812 control
+duckgp2=gpio42
+# max brightness (0-255)
+brightness=128
+# animation speed (lower=faster)
+speed=20
+
+def demo_ws2812():
+    np = neopixel.NeoPixel(duckgp2, 2)
+    c=1
+    while True:
+        if c&1:
+            r = 0xff
+        else:
+            r = 0
+        if c&2:
+            g = 0xff
+        else:
+            g = 0
+        if c&4:
+            b = 0xff
+        else:
+            b = 0
+        v = 0
+        while (v < brightness):
+            # background LED, only B-channel is connected
+            np[0] = (0, 0, v)
+            # RGB LED
+            np[1] = (v&r, v&g, v&b)
+            np.write()
+            v += 1
+            time.sleep_ms(speed)
+        while (v > 0):
+            np[0] = (0, 0, v)
+            np[1] = (v&r, v&g, v&b)
+            np.write()
+            v -= 1
+            time.sleep_ms(speed)
+        c += 1
+        if (c > 7):
+            c = 1
+
+demo_ws2812()
